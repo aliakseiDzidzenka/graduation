f437dc16d823f859f2cc2b7a470099b491731e2e
Aliaksei Dzidzenka <aliakseidzidzenka@gmail.com>
2019-05-18 21:50:19 +0300

    0 at crystal quanity by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_18_093122) do
+ActiveRecord::Schema.define(version: 2019_05_18_184755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "crystals", force: :cascade do |t|
     t.bigint "user_id"
-    t.integer "quantity"
+    t.integer "quantity", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_crystals_on_user_id"
