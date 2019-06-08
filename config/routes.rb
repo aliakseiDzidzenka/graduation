1deb9daf216ac077d5097df002dceba0ada70273
Aliaksei Dzidzenka <aliakseidzidzenka@gmail.com>
2019-06-08 22:08:30 +0300

    sending crystals als withoutform and fading alerts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'game/plane'
   get 'game/test'
 
+  post 'game/par'
+  #post 'crys/par'
+
+
   devise_for :users
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
